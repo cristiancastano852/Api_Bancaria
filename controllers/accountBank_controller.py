2a392e61014fd9d84a494545f939e05cf3e3e0b7
Cristian Castaño <cristiancastano852@gmail.com>
2024-04-28 12:36:38 -0500

    Docs: Add docstring to Account Banks methods

--- controllers/accountBank_controller.py
+++ controllers/accountBank_controller.py
@@ -7,56 +7,102 @@ class AccountBankController:
     def __init__(self, account_service: AccountBankService):
         self.account_service = account_service
 
-
     def create_account(self, account: AccountBank) -> str:
+        """
+        Create a new account.
+
+        Parameters:
+        - account (AccountBank): The AccountBank object representing the account to be created.
+
+        Returns:
+        - str: The ID of the newly created account.
+
+        """
         new_account = dict(account)
         created_account = self.account_service.create_account(new_account)
         return created_account
 
     def replace_balance(self, account_id: str, balance: float) -> AccountBank:
-        updated_account = self.account_service.replace_balance(account_id, balance)
+        """
+        Replace the balance of an account.
+
+        Parameters:
+        - account_id (str): The ID of the account to replace the balance.
+        - balance (float): The new balance to replace the current balance.
+
+        Returns:
+        - AccountBank: The updated AccountBank object representing the account with the replaced balance.
+
+        """
+        updated_account = self.account_service.replace_balance(
+            account_id, balance)
         return updated_account
-    
+
     def update_balance(self, account_id: str, amount: float) -> str:
-        updated_account = self.account_service.update_balance(account_id, amount)
+        """
+        Update the balance of an account.
+
+        Parameters:
+        - account_id (str): The ID of the account to update.
+        - amount (float): The amount by which to update the balance.
+
+        Returns:
+        - str: The updated account information.
+
+        """
+        updated_account = self.account_service.update_balance(
+            account_id, amount)
         return updated_account
-    
+
     def get_all_accounts(self) -> List[AccountBank]:
+        """
+        Get all accounts.
+
+        Returns:
+        - List[AccountBank]: A list of AccountBank objects representing all the accounts.
+
+        """
         accounts = self.account_service.get_all_accounts()
         return accounts
-    
+
     def get_account(self, account_id: str) -> AccountBank:
+        """
+        Get an account by its ID.
+
+        Parameters:
+        - account_id (str): The ID of the account to retrieve.
+
+        Returns:
+        - AccountBank: The AccountBank object representing the account with the specified ID.
+
+        """
         account = self.account_service.get_account(account_id)
         return account
-    
+
     def delete_account(self, account_id: str) -> int:
+        """
+        Delete an account.
+
+        Parameters:
+        - account_id (str): The ID of the account to be deleted.
+
+        Returns:
+        - int: The number of accounts deleted (should be 1 if successful).
+
+        """
         deleted_count = self.account_service.delete_account(account_id)
         return deleted_count
-    
+
     def get_account_by_user(self, user_id: str) -> List[AccountBank]:
+        """
+        Get all accounts associated with a specific user.
+
+        Parameters:
+        - user_id (str): The ID of the user.
+
+        Returns:
+        - List[AccountBank]: A list of AccountBank objects representing the accounts associated with the user.
+
+        """
         accounts = self.account_service.get_account_by_user(user_id)
         return accounts
-
-    # def __init__(self, user_service: UserService):
-    #     self.user_service = user_service
-
-    # def get_user(self, user_id: str) -> User:
-    #     user = self.user_service.get_user(user_id)
-    #     return user
-
-    # def get_all_users(self) -> List[User]:
-    #     users = self.user_service.get_all_users()
-    #     return users
-
-    # def create_user(self, user_data: User) -> User:
-    #     new_user = dict(user_data)
-    #     created_user = self.user_service.create_user(new_user)
-    #     return created_user
-
-    # def update_user(self, user_id: str, user: User) -> User:
-    #     updated_user = self.user_service.update_user(user_id, user)
-    #     return updated_user
-
-    # def delete_user(self, user_id: str) -> int:
-    #     deleted_count = self.user_service.delete_user(user_id)
-    #     return deleted_count
--- services/accountBank_service.py
+++ services/accountBank_service.py
@@ -1,35 +1,59 @@
 from datetime import datetime
-from models.accountBank import AccountBank
 from db.mongodb import conn
 from bson import ObjectId
 from schemas.accountBack import accountsEntity, accountEntity
-from fastapi import APIRouter, Response, status
+from fastapi import Response
 from starlette.status import HTTP_204_NO_CONTENT
 from fastapi.exceptions import HTTPException
 
 
 class AccountBankService:
     def is_valid_objectid(cls, user_id: str):
+        """
+        Check if the provided user_id is a valid ObjectId.
+
+        Parameters:
+            user_id (str): The user_id to be checked.
+
+        Returns:
+            bool: True if the user_id is a valid ObjectId, False otherwise.
+        """
         try:
             ObjectId(user_id)
             return True
         except Exception:
             return False
-    
+
     def create_account(self, new_account_data: dict):
+        """
+        Create a new account with the provided account data.
+
+        Parameters:
+            new_account_data (dict): A dictionary containing the data for the new account.
+
+        Returns:
+            dict: A dictionary containing the ID of the newly created account.
+
+        Raises:
+            HTTPException: If the user with the specified user_id is not found or if there is an error during the account creation process.
+        """
         try:
             del new_account_data["id"]
             user_id = new_account_data["user_id"]
             if not self.is_valid_objectid(user_id):
-                raise HTTPException(status_code=404, detail=f"User with id <{user_id}> no found")
+                raise HTTPException(
+                    status_code=404, detail=f"User with id <{user_id}> no found")
             user = conn.bancaria.users.find_one({"_id": ObjectId(user_id)})
             if not user:
-                raise HTTPException(status_code=404, detail=f"User with id <{user_id}> no found")
+                raise HTTPException(
+                    status_code=404, detail=f"User with id <{user_id}> no found")
             result = conn.bancaria.accounts.insert_one(new_account_data)
             inserted_id = result.inserted_id
             return {"id": str(inserted_id)}
+        except HTTPException as e:
+            raise e
         except Exception as e:
-            raise HTTPException(status_code=500, detail=str(e))
+            raise Exception("Error creating account ", str(e))
 
     def replace_balance(self, account_id: str, balance: float):
         """
@@ -51,7 +75,7 @@ class AccountBankService:
                 "updated_at": datetime.now()  # Update the updated_at field
             }
             conn.bancaria.accounts.update_one({"_id": ObjectId(account_id)}, {
-                                            "$set": update_data})
+                "$set": update_data})
             updated_account = conn.bancaria.accounts.find_one(
                 {"_id": ObjectId(account_id)})
             return accountEntity(updated_account)
@@ -89,7 +113,8 @@ class AccountBankService:
                     {"_id": ObjectId(account_id)})
                 return {"Saldo actualizado": accountEntity(updated_account)}
             else:
-                raise HTTPException(status_code=404, detail="Account not found")
+                raise HTTPException(
+                    status_code=404, detail="Account not found")
         except Exception as e:
             raise Exception("Error updating balance: ", str(e))
 
@@ -97,16 +122,42 @@ class AccountBankService:
         return accountsEntity(conn.bancaria.accounts.find())
 
     def get_account(self, account_id: str):
+        """
+        Get the account with the specified account_id.
+
+        Parameters:
+            account_id (str): The unique identifier of the account.
+
+        Returns:
+            dict: A dictionary representing the account with the specified account_id.
+
+        Raises:
+            HTTPException: If the account with the specified account_id is not found.
+            Exception: If there is an error getting the account.
+        """
         try:
             account = conn.bancaria.accounts.find_one(
                 {"_id": ObjectId(account_id)})
             if not account:
-                raise HTTPException(status_code=404, detail="Account not found")
+                raise HTTPException(
+                    status_code=404, detail="Account not found")
             return accountEntity(account)
         except Exception as e:
             raise Exception("Error getting account: ", str(e))
 
     def get_account_by_user(self, user_id: str):
+        """
+        Get all accounts associated with a user.
+
+        Parameters:
+            user_id (str): The unique identifier of the user.
+
+        Returns:
+            list: A list of accountEntity objects representing the accounts associated with the user.
+
+        Raises:
+            Exception: If there is an error retrieving the accounts.
+        """
         try:
             accounts = conn.bancaria.accounts.find({"user_id": user_id})
             return accountsEntity(accounts)
@@ -114,9 +165,27 @@ class AccountBankService:
             raise Exception("Error getting accounts: ", str(e))
 
     def delete_account(self, account_id: str):
+        """
+        Delete an account with the specified account_id.
+
+        Parameters:
+            account_id (str): The unique identifier of the account to be deleted.
+
+        Returns:
+            Response: A response indicating the success or failure of the deletion operation.
+
+        Raises:
+            HTTPException: If the account with the specified account_id is not found.
+            Exception: If there is an error during the deletion process.
+        """
         try:
             result = conn.bancaria.accounts.delete_one(
                 {"_id": ObjectId(account_id)})
+            if result.deleted_count == 0:
+                raise HTTPException(
+                    status_code=404, detail="Account not found")
             return Response(status_code=HTTP_204_NO_CONTENT)
+        except HTTPException as e:
+            raise e
         except Exception as e:
             raise Exception("Error deleting account: ", str(e))
