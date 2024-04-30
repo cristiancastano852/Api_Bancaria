8b5d737e6746340e6d8ccc6828a9289bbf80edd3
Cristian Castaño <cristiancastano852@gmail.com>
2024-04-29 22:02:33 -0500

    Quick fix: load envs

--- db/mongodb.py
+++ db/mongodb.py
@@ -1,7 +1,9 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
 import os
 
+load_dotenv()
 uri = os.getenv('MONGODB_URI')
 
 # Create a new client and connect to the server
